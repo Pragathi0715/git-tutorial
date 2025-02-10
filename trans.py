3cb35bf5d3ab3ff24f30748a5fbeb5e9e681adb8
Pragathi <pragathivrp@gmail.com>
2025-02-10 19:53:23 +0530

    my commit

---
+++ trans.py
@@ -0,0 +1,24 @@
+kilometres = float(input("Enter the distance in kilometres: "))
+price = int(input("Enter the price: "))
+
+if price > 1000 and kilometres > 700:
+    mode = "Car and Bike"
+elif  price < 500 and kilometres < 699:
+    mode = "Bus and train"
+else:
+    print("No suitable transportation available")
+
+signal = input("Enter the traffic signal color (red/green/orange): ")
+
+if signal == "red":
+    action = "Stop"
+elif signal == "green":
+    action = "Run"
+elif signal == "orange":
+    action = "Pause"
+else:
+    action = "Invalid signal"
+
+if mode !="no sutiable transport is available":
+     print(f"For {kilometres} km at rupees {price} price, the mode of transportation is: {mode}.")
+     print(f"Signal is :{signal}. Action is : {action}.")
